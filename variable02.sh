cca258c9709ea88de5d4c50e5dd2084d86b0aac6
bavikadi <Prashanthbavikadiias@.com>
2025-03-16 08:42:36 +0530

    some-message

---
+++ variable02.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+#declaring the variable
+person1=Anji #no space b/w person adn Anji
+person2=Prashanth
+person3=lahari
+
+#refering the variable
+
+ echo "Anji : Hi prashanth how are you ?"
+ echo "lahari : Anji I LOVE YOU BAVA"
+ echo "Anji: I LOVE YOU TOO lahari"
+ echo "Prashanth: Hi Anji im fine "
+ echo "Anji: What are you preparing for "
+ echo "Prashanth : im preaparing for devops"
