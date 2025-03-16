4cc2ef72f299f4cc2b63fd5c9139e148cd09444a
bavikadi <Prashanthbavikadiias@.com>
2025-03-16 08:26:14 +0530

    some-message

---
+++ Varaibles.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+
+ echo "Sandeep : Hi prashanth how are you ?"
+ echo "Prashanth: Hi sandeep im fine "
+ echo "Sandeep: What are you preparing for "
+ echo "Prashanth : im preaparing for devops"
