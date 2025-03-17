43777ab7dcb9c575a41c63fa7eb8e7fa37928d16
bavikadi <Prashanthbavikadiias@.com>
2025-03-18 02:31:36 +0530

    some-message

--- cond-vari-08.sh
+++ cond-vari-08.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-echo " enter you AGE"
-read AGE
- 
-if [ $AGE -ge 18 ]
+$NUMBER=1$
+
+if [ $NUMBER -ge 15 ]
 then
-    echo " you are elgible for vote"
-else 
-    echo "you are not eligible to vote" 
-fi
+    echo " Provided $NUMBER is greater that 15 "
+else    
+    echo " Provided $NUMBER id lesser that 15"
+fi    
+
+
+
+
+
+
+#echo " enter you AGE"
+#read AGE
+ 
+#if [ $AGE -ge 18 ]
+#then
+    #echo " you are elgible for vote"
+#else 
+    #echo "you are not eligible to vote" 
+#fi
 
