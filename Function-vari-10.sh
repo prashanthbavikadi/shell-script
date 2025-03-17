d19423ff2fed1da35cc80bd4fa88c6aff871a678
bavikadi <Prashanthbavikadiias@.com>
2025-03-18 03:15:42 +0530

    some-message

---
+++ Function-vari-10.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VADIDATE (){
+if [ $? - ne 0]
+then 
+    echo " installation is ....FAILURE."
+    exit 1
+else
+    echo " installation is ....SUCCESS."
+fi
+
+if [ $USERID -ne 0 ]
+then
+    echo " please run this command with root access."
+    exit 2   # manually exit if error comes.
+else 
+    echo " you are in root access."
+fi
+dnf install mysql -y &>>>$LOGFILE
+VALIDATE $?  " Installing is =mysql"
+
+dnf install git -y &>>>$LOGFILE
+VALIDATE $?  " Installing is git"
