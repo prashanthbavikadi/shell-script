b330f17aca7fdfc6d77215d253f9128d2290fb26
bavikadi <Prashanthbavikadiias@.com>
2025-03-17 15:10:36 +0530

    some-message

--- sumVaraible-06.sh
+++ sumVaraible-06.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-    NO1=$1
-    NO2=$2
+NO1=$1
+NO2=$2
 
-    SUM=$(($NO1+$NO2))
+SUM=$(($NO1+$NO2))
 
-    echo " the sum of the 2 numbers are: $SUM"
+echo "Total of $NO1 and $NO2 is: $SUM"
