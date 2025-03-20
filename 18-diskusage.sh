20003c097acb622a460c92c568a7eb8389e6bd29
bavikadi <Prashanthbavikadiias.com>
2025-03-20 13:11:28 +0530

    some-message

--- 18-diskusage.sh
+++ 18-diskusage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_TRESHOLD=10
+DISK_THRESHOLD=6
+MESSAGE=""
 
 while IFS= read -r line
-do 
-    USAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1 )
-    FOLDER=$(echo $line |  awk -F " " '{print $Nf}' )
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$FOLDER is more than $DISK_THRESHOLD,  current usage:  $USAGE"
-    fi 
-
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+    fi
 done <<< $DISK_USAGE
