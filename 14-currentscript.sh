47b818e550e61cfe2243fb5ca56065a25558a0af
bavikadi <Prashanthbavikadiias.com>
2025-03-20 07:54:20 +0530

    some-message

--- 14-currentscript.sh
+++ 14-currentscript.sh
@@ -5,6 +5,8 @@ JOB="DEVOPS FROM Current Company"
 echo "Before calling  other Company , job: $JOB"
 echo "process id of current script: $$"
 
-./15-otherscript.sh
+#./15-otherscript.sh
+
+source./15-otherscript.sh
 
 echo "After calling  other Company , job: $JOB"
