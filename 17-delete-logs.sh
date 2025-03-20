5d194e981da044f6df1514cc2e4651a13897ef4a
bavikadi <Prashanthbavikadiias.com>
2025-03-20 11:45:57 +0530

    some-message

---
+++ 17-delete-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+     echo "$G source directory exits $N"
+else
+     echo "$Y please create $SOURCE_DIRECTORY exits $N"
+fi       
+
+    
