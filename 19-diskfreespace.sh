0b5d37d0e7c16221d225be9c1554b71c7754709c
bavikadi <Prashanthbavikadiias.com>
2025-03-21 03:32:41 +0530

    some-message

--- 19-diskfreespace.sh
+++ 19-diskfreespace.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 DISK_SPACE=( df -hT)
-FREE_THRESHOLD=
+
 
 while IFS= read -r line
 do
-    SPACE=$(echo $line | grep xfs | awk -F " " '{print $5}')
+    DISK_SPACE=$(echo $line | grep xfs | awk -F " " '{print $5}')
     
 done <<< $DISK_SPACE 
