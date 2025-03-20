5f843f0598fb3624b1f338b8b53a7b29525a681b
bavikadi <Prashanthbavikadiias.com>
2025-03-20 07:57:49 +0530

    some-message

--- 14-currentscript.sh
+++ 14-currentscript.sh
@@ -6,6 +6,7 @@ echo "Before calling  other Company , job: $JOB"
 echo "process id of current script: $$"
 
 #./15-otherscript.sh
+# use of source is it will takes variable from osther srcipt % include o/p both scripts
 
 source ./15-otherscript.sh
 
