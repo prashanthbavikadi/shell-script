a398b6ccbc3b63574ac81dbc00482a359af7d883
bavikadi <Prashanthbavikadiias.com>
2025-03-20 13:07:36 +0530

    some-message

--- 18-diskusage.sh
+++ 18-diskusage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DISKUSAGE=$( df -hT | grep xfs)
+DISK_USAGE=$( df -hT | grep xfs)
 DISK_TRESHOLD=15
 
 while IFS= read -r line
 do 
-    DISKUSAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1 )
+    USAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1 )
     FOLDER=$(echo $line |  awk -F " " '{print $Nf}' )
-    if [ $DISKUSAGE -ge $DISK_THRESHOLD ]
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo "$FOLDER is more than $DISK_THRESHOLD,  current usage:  $USAGE"
     fi 
 
-done <<< $DISKUSAGE
+done <<< $DISK_USAGE
