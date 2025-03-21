cae3ab0c1ab3714890d75526230f5b873ad1f9af
bavikadi <Prashanthbavikadiias.com>
2025-03-21 06:43:39 +0530

    some-message

--- 20-cpu.sh
+++ 20-cpu.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-CPU_PROCESS=$(top  -o %CPU )
-MESSAGE=""
 
-while IFS= read -r line
-do 
-   PROCESS=$(echo  $LINE | head -n 12 | awk -F " " '{print $3 $1}')
-   COMMAND=$(echo  $LINE | awk -F " " '{print $NF}' )
+CPU_PROCESS=$(top  -o %CPU | head -n 12)
+
+
+
+#while IFS= read -r line
+#do 
+   #PROCESS=$(echo  $LINE | head -n 12 | awk -F " " '{print $10}')
+   #COMMAND=$(echo  $LINE | awk -F " " '{print $NF}' )
    
-done <<< CPU_5PROCESS
+#done <<< CPU_5PROCESS
 
-MESSAGE+="$COMMAND is more than $CPU_PROCESS \n"
+#MESSAGE+="$COMMAND is more than $CPU_PROCESS \n"
