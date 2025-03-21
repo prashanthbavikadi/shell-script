44c4c1315df55630478cc7a7c3e52ddfd630a521
bavikadi <Prashanthbavikadiias.com>
2025-03-21 13:35:50 +0530

    some-message

--- 22-backup.sh
+++ 22-backup.sh
@@ -9,7 +9,7 @@ G="\e[32m"
 
 if [ -d "$SOURCE_DIRECTORY" ]; 
 then
-    echo "Error: Source directory '$SOURCE_DIR' does not exist."
+    echo "Error: Source directory '$SOURCE_DIRECTORY' does not exist."
     exit 1
 else    
     echo "$Y please create $SOURCE_DIRECTORY exits $N"
@@ -17,7 +17,7 @@ fi
 
 if [ -d "$DEST_DIRECTORY" ]; 
 then
-    echo "Error: Source directory '$DEST_DIR' does not exist."
+    echo "Error: Source directory '$DEST_DIRECTORY' does not exist."
     exit 1
 else    
     echo "$Y please create $DEST_DIRECTORY exits $N"
