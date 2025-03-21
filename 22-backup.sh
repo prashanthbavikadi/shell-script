03bac3d25478b63cadfd45f250fc3e91d2cfc613
bavikadi <Prashanthbavikadiias.com>
2025-03-21 14:24:42 +0530

    some-message

--- 22-backup.sh
+++ 22-backup.sh
@@ -9,19 +9,19 @@ G="\e[32m"
 
 if [ -d $SOURCE_DIRECTORY ]
 then
-    echo "Source directory $SOURCE_DIRECTORY does not exist."
+    echo "Source directory $SOURCE_DIRECTORY  exits."
     
 else    
-    echo "$Y please create $SOURCE_DIRECTORY exits $N"
-    exit 1
+    echo "$Y please create $SOURCE_DIRECTORY does not  exits $N"
+    
 fi
 
 if [ -d $DEST_DIRECTORY ]
 then
-    echo "Source directory $DEST_DIRECTORY does not exist."
+    echo " destination directory $DEST_DIRECTORY  exist."
     exit 1
 else    
-    echo "$Y please create $DEST_DIRECTORY exits $N"
+    echo "$Y please create $DEST_DIRECTORY does not exits $N"
 fi
 
 #BACKUP_FILE="backup_$( "$SOURCE_DIRECTORY ")_$TIMESTAMP.tar.gz
