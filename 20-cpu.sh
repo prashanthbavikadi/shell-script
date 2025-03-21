a9947f3c5e52610ea7efac01fb9a50fa3f8dbbec
bavikadi <Prashanthbavikadiias.com>
2025-03-21 06:56:10 +0530

    some-message

--- 20-cpu.sh
+++ 20-cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-CPU_PROCESS=$(ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%mem | head -n 8 | awk -F " " '{print $4F}'  | cut -d "%" -f1 )
+CPU_PROCESS=$(ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%mem | head -n 8 | awk -F " " '{print $3F}'  | cut -d "%" -f1 )
 
 echo "this  shows 5 CPU processing : $CPU_PROCESS "
 
