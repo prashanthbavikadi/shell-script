c84de1b0ab5c26050c18b6c76c204cebccade3b6
bavikadi <Prashanthbavikadiias.com>
2025-03-21 14:19:37 +0530

    some-message

--- 22-backup.sh
+++ 22-backup.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-SOURCE_DIRECTORY=/tmp/file
+SOURCE_DIRECTORY=/home/ec2-user/source
 DEST_DIRECTORY=/home/ec2-user/dest
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 R="\e[31m"
 G="\e[32m"
 
-if [ -d $SOURCE_DIRECTORY ];
+if [ -d $SOURCE_DIRECTORY ]
 then
-    echo "Error: Source directory $SOURCE_DIRECTORY does not exist."
-    exit 1
+    echo "Source directory $SOURCE_DIRECTORY does not exist."
+    
 else    
     echo "$Y please create $SOURCE_DIRECTORY exits $N"
+    exit 1
 fi
 
-if [ -d $DEST_DIRECTORY]; 
+if [ -d $DEST_DIRECTORY]
 then
-    echo "Error: Source directory $DEST_DIRECTORY does not exist."
+    echo "Source directory $DEST_DIRECTORY does not exist."
     exit 1
 else    
     echo "$Y please create $DEST_DIRECTORY exits $N"
