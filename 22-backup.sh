002281b330c7bc2191f786e7124d6170f2bbf105
bavikadi <Prashanthbavikadiias.com>
2025-03-21 14:07:01 +0530

    some-message

--- 22-backup.sh
+++ 22-backup.sh
@@ -7,17 +7,17 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 
-if [ -d "$SOURCE_DIRECTORY" ]; 
+if [ -d $SOURCE_DIRECTORY ];
 then
-    echo "Error: Source directory '$SOURCE_DIRECTORY' does not exist."
+    echo "Error: Source directory $SOURCE_DIRECTORY does not exist."
     exit 1
 else    
     echo "$Y please create $SOURCE_DIRECTORY exits $N"
 fi
 
-if [ -d "$DEST_DIRECTORY" ]; 
+if [ -d $DEST_DIRECTORY]; 
 then
-    echo "Error: Source directory '$DEST_DIRECTORY' does not exist."
+    echo "Error: Source directory $DEST_DIRECTORY does not exist."
     exit 1
 else    
     echo "$Y please create $DEST_DIRECTORY exits $N"
