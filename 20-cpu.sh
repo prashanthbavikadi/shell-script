277f9056187ee8add3a46de3c67ffc6b091e4479
bavikadi <Prashanthbavikadiias.com>
2025-03-21 06:14:32 +0530

    some-message

--- 20-cpu.sh
+++ 20-cpu.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 CPU_5PROCESS=$(top  -o %CPU)
+MESSAGE=""
 
 while IFS= read -r line
 do 
-   PROCESS=$(echo top -o %CPU -n 1 | head -n 12)
+   PROCESS=$(echo  $LINE | head -n 12 | awk -F " " '{print$1 " "  $10 " " $11}')
+   COMMAND=$(echo  $LINE | awk -F " " '{print $NF}' )
    
 done <<< CPU_5PROCESS
+
+MESSAGE+="$COMMAND is more than $CPU_5PROCESS \n"
