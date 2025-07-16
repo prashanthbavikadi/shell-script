5baf390483d5378f78311150485fc7709fea487e
prashanthbavikadi <bavikadiprashanthias@gmail.com>
2025-07-16 10:10:44 +0530

    aws-service

--- 20-cpu.sh
+++ 20-cpu.sh
@@ -6,11 +6,11 @@ CPU_PROCESS=$(ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%mem | head -n 8 | awk -F " 
 echo "this  shows 5 CPU processing : $CPU_PROCESS "
 
 
-#while IFS= read -r line
-#do 
-   #PROCESS=$(echo  $LINE | head -n 12 | awk -F " " '{print $10}')
-   #COMMAND=$(echo  $LINE | awk -F " " '{print $NF}' )
+while IFS= read -r line
+do 
+   PROCESS=$(echo  $LINE | head -n 12 | awk -F " " '{print $10}')
+   COMMAND=$(echo  $LINE | awk -F " " '{print $NF}' )
    
-#done <<< CPU_5PROCESS
+done <<< CPU_5PROCESS
 
-#MESSAGE+="$COMMAND is more than $CPU_PROCESS \n"
+MESSAGE+="$COMMAND is more than $CPU_PROCESS \n"
